560ef887a081cb842b3e1a6aabc300b163e04a74
Hongkun Jin <hongkun.jin@sprint.com>
2021-02-25 10:51:19 -0600

    init project

---
+++ app/src/main/java/com/example/adopuppymax/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.example.adopuppymax.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = max_color_main_light,
+    primaryVariant = max_color_main_dark,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = max_color_main_mid,
+    primaryVariant = max_color_main_dark,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun AdopuppymaxTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
